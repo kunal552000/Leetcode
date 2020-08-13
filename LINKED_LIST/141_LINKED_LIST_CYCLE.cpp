9e9b0f3fa4d10530b4d64feb5ae9ceb93d2b2fab
kunal552000 <1906106@kiit.ac.in>
2020-08-13 21:12:43 +0530

    cpp code

---
+++ LINKED_LIST/141_LINKED_LIST_CYCLE.cpp
@@ -0,0 +1,41 @@
+//Given a linked list, determine if it has a cycle in it.
+
+//To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+
+
+//Example 1:
+
+//Input: head = [3,2,0,-4], pos = 1
+//Output: true
+//Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+
+//CODE :-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+        struct ListNode *slow,*fast;
+        slow = head;
+        fast = head;
+        while(slow && fast && fast->next)
+        {
+
+            fast = fast->next->next;
+             slow = slow->next;
+            if(slow==fast)
+                return true;
+        }
+        return false;
+
+    }
+};
