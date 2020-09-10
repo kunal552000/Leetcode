f4722ccd957dae6d84b947153620e148b713acc0
kunal552000 <1906106@kiit.ac.in>
2020-09-10 14:20:16 +0530

    cpp code

---
+++ BINARY TREE/101_SYMMETRIC_TREE.cpp
@@ -0,0 +1,55 @@
+/*
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ 
+
+Follow up: Solve it both recursively and iteratively.
+
+*/
+
+//CODE
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL)
+            return true;
+        return mirror(root->left,root->right);
+        
+    }
+   bool mirror( TreeNode* p, TreeNode* q){
+        if(!p && !q)
+            return true;
+        if(!p || !q)
+            return false;
+        return p->val==q->val && mirror(p->left,q->right) && mirror(p->right,q->left);
+    }
+};
