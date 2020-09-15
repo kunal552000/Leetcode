8f3d99aa58326e52d582ab68f4b7400882bfd3f0
kunal552000 <1906106@kiit.ac.in>
2020-09-15 19:30:45 +0530

    cpp code

---
+++ BINARY TREE/513_BOTTOM_LEFT_TREE_VALUE.cpp
@@ -0,0 +1,61 @@
+/*
+
+Given a binary tree, find the leftmost value in the last row of the tree.
+
+Example 1:
+Input:
+
+    2
+   / \
+  1   3
+
+Output:
+1
+Example 2:
+Input:
+
+        1
+       / \
+      2   3
+     /   / \
+    4   5   6
+       /
+      7
+
+Output:
+7
+Note: You may assume the tree (i.e., the given root node) is not NULL.
+
+*/
+
+//CODE
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode *>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            root = q.front();
+            q.pop();
+            if(root->right)
+                q.push(root->right);
+            if(root->left)
+                q.push(root->left);
+        }
+        return root->val;
+        
+    }
+};
