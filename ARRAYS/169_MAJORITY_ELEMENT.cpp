23f79cb312c9019d1a7af837e807abbb9f7ed508
kunal552000 <1906106@kiit.ac.in>
2020-10-07 19:18:17 +0530

    cpp code

---
+++ ARRAYS/169_MAJORITY_ELEMENT.cpp
@@ -0,0 +1,38 @@
+/*
+
+Given an array of size n, find the majority element. The majority element is the element that appears more than ? n/2 ? times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+
+*/
+
+//CODE
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int majority = 0;
+        for(int i : nums)
+        {
+            if(count == 0)
+                majority = i;
+            if(i == majority)
+                count++;
+            else
+                count--;
+            
+        }
+        return majority;
+        
+    }
+};
