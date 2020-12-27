8add5846c85a97b81147dcd2260a80c231417a68
kunal552000 <1906106@kiit.ac.in>
2020-12-27 19:05:06 +0530

    cpp code

---
+++ ARRAYS/217_CONTAINS_DUPLICATES.cpp
@@ -0,0 +1,41 @@
+/*
+
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+Example 3:
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+*/
+
+//CODE
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+            if(s.find(nums[i])!=s.end())
+                return true;
+            else
+                s.insert(nums[i]);
+        }
+        if(nums.size() == s.size())
+            return false;
+        return true;
+        
+        
+    }
+};
