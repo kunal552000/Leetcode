b5c3c704dfa5a078a313bba9223e50ed08f0684e
kunal552000 <1906106@kiit.ac.in>
2021-02-04 16:01:17 +0530

    sql

---
+++ SQL_HACKERANK/JAPANESE_CITIES_ATTRIBUTES.sql
@@ -0,0 +1,11 @@
+/*
+
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+The CITY table is described as follows:
+
+*/
+
+/* CODE */
+
+select * from CITY where COUNTRYCODE = "JPN";
