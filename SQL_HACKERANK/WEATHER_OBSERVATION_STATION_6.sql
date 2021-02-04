b933d39a7e4c861691dafb2f832e8fd0e77c53d5
kunal552000 <1906106@kiit.ac.in>
2021-02-04 16:07:52 +0530

    sql

---
+++ SQL_HACKERANK/WEATHER_OBSERVATION_STATION_6.sql
@@ -0,0 +1,13 @@
+/*
+
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+*/
+
+/*CODE*/
+
+SELECT DISTINCT city FROM station WHERE city REGEXP "^[aeiou]";
