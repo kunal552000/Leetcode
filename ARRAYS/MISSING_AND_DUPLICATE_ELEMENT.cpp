82555e57551adcb10f2e9606bdd6d0cd6b7d45f3
kunal552000 <1906106@kiit.ac.in>
2021-08-02 10:20:20 +0530

    cpp code

---
+++ ARRAYS/MISSING_AND_DUPLICATE_ELEMENT.cpp
@@ -0,0 +1,73 @@
+/*
+
+Given an unsorted array Arr of size N of positive integers. One number 'A' from set {1, 2, …N} is missing and one number 'B' 
+occurs twice in array. Find these two numbers.
+
+Example 1:
+
+Input:
+N = 2
+Arr[] = {2, 2}
+Output: 2 1
+Explanation: Repeating number is 2 and 
+smallest positive missing number is 1.
+Example 2:
+
+Input:
+N = 3
+Arr[] = {1, 3, 3}
+Output: 3 2
+Explanation: Repeating number is 3 and 
+smallest positive missing number is 2.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function findTwoElement() which takes the 
+array of integers arr and n as parameters and returns an array of integers of size 2 denoting the answer 
+( The first index contains B and second index contains A.)
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 = N = 105
+1 = Arr[i] = N
+
+*/
+
+//CODE
+
+
+
+class Solution{
+public:
+    int *findTwoElement(int *arr, int n) {
+        // code here
+        
+        int *ans = new int(2);
+        
+        int i = 0;
+        while(i < n)
+        {
+            if(arr[i] != arr[arr[i]-1])
+            swap(arr[i] , arr[arr[i]-1]);
+            
+            else
+            i++;
+            
+            
+           
+            
+        }
+        
+        for(int j = 0; j < n ; j++)
+        {
+            if(arr[j] != j+1)
+            {
+                ans[0] = arr[j];
+                ans[1] = j+1;
+                return ans;
+            }
+        }
+        
+        return ans;
+    }
+};
