f5f47fe44a2098758846d6d31c0c58b7c37f8e4c
kunal552000 <1906106@kiit.ac.in>
2021-09-07 09:23:55 +0530

    cpp code

---
+++ BINARY TREE/MORRIS_PREORDER.cpp
@@ -0,0 +1,104 @@
+/*
+
+
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [1,2]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+
+*/
+
+
+//CODE
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        
+        if(!root)
+            return res;
+        
+        TreeNode* curr = root;
+        
+        while(curr)
+        {
+            if(!curr->left)
+            {
+                res.push_back(curr->val);
+                curr = curr->right;
+            }
+            
+            else
+            {
+                TreeNode* pred = curr->left;
+                
+                while(pred->right && pred->right != curr)
+                {
+                    pred = pred->right;
+                }
+                
+                if(pred->right == NULL)
+                {
+                    pred->right = curr;
+                    res.push_back(curr->val);
+                    curr = curr->left;
+                }
+                
+                else
+                {
+                    pred->right = NULL;
+                    
+                    curr = curr->right;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
