96da81d92e88b87044086c42a20fa30a81c3ca8c
kunal552000 <1906106@kiit.ac.in>
2021-09-10 10:09:24 +0530

    cpp code

---
+++ DP/64_MIN_PATH_SUM.cpp
@@ -0,0 +1,64 @@
+/*
+
+
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of 
+all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+Output: 7
+Explanation: Because the path 1 ? 3 ? 1 ? 1 ? 1 minimizes the sum.
+Example 2:
+
+Input: grid = [[1,2,3],[4,5,6]]
+Output: 12
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 200
+0 <= grid[i][j] <= 100
+
+
+*/
+
+
+//CODE
+
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        int memo[m][n];
+        
+        memo[0][0] = grid[0][0];
+        
+        for(int i = 1; i < m; i++)
+            memo[i][0] = grid[i][0] + memo[i-1][0];
+        
+        
+        for(int j = 1; j < n ; j++)
+            memo[0][j] = grid[0][j] + memo[0][j-1];
+        
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n ; j++)
+            {
+                memo[i][j] = grid[i][j] + min(memo[i-1][j],memo[i][j-1]);
+            }
+        }
+        
+        return memo[m-1][n-1];
+    }
+};
