5f2cb1347bfc281d087c599cb18c27bc9a533cfc
kunal552000 <1906106@kiit.ac.in>
2021-11-13 09:15:05 +0530

    cpp code

---
+++ LINKED_LIST/109_CONVERT_SORTED_LIST_TO_BST.cpp
@@ -0,0 +1,96 @@
+/*
+
+Given the head of a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node 
+never differ by more than 1.
+
+ 
+
+Example 1:
+
+
+Input: head = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST.
+Example 2:
+
+Input: head = []
+Output: []
+Example 3:
+
+Input: head = [0]
+Output: [0]
+Example 4:
+
+Input: head = [1,3]
+Output: [3,1]
+ 
+
+Constraints:
+
+The number of nodes in head is in the range [0, 2 * 104].
+-105 <= Node.val <= 105
+
+*/
+
+//CODE
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        
+        if(!head)
+            return NULL;
+        
+        if(!head->next)
+            return new TreeNode(head->val);
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        TreeNode* root = new TreeNode(slow->val);
+        
+        ListNode* p = head;
+        while(p->next != slow)
+        {
+            p = p->next;
+        }
+        
+        p->next = NULL;
+        
+        
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(slow->next);
+        
+        return root;
+    }
+};
