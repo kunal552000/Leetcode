bd88424c459ffd56115f5ca90342ef5efdf24c36
kunal552000 <1906106@kiit.ac.in>
2022-01-05 18:21:16 +0530

    cpp code

---
+++ SLIDING_WINDOW/209_MINIMUM_SIZE_SUBARRAY_SUM.cpp
@@ -0,0 +1,89 @@
+/*
+
+
+Given an array of positive integers nums and a positive integer target, return the minimal length of a contiguous 
+subarray [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target. If there is no such subarray, 
+return 0 instead.
+
+ 
+
+Example 1:
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+Example 2:
+
+Input: target = 4, nums = [1,4,4]
+Output: 1
+Example 3:
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+ 
+
+Constraints:
+
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 105
+ 
+
+Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
+
+*/
+
+
+//CODE
+
+
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        
+        int res = INT_MAX;
+        
+        int i = 0;
+        int j = 0;
+        
+        int sum = 0;
+        
+        int n = nums.size();
+        
+        while(j < n)
+        {
+            sum = sum + nums[j];
+            
+            if(sum < s)
+                j++;
+            
+            else if(sum == s)
+            {
+                res = min(res,j-i+1);
+                j++;
+            }
+            
+            else if(sum > s)
+            {
+                while(sum > s)
+                {
+                    res = min(res,j-i+1);
+                   
+                    
+                    sum = sum - nums[i];
+                    i++;
+                    if(sum == s)
+                        res = min(res,j-i+1);
+                }
+                
+                j++;
+            }
+        }
+        
+        if(res == INT_MAX)
+            return 0;
+        
+        return res;
+        
+    }
+};
